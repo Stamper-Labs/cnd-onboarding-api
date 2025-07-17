e907bdaf36d308b0ed2e8a69b45aa3c4f75b6dcc
Captain Mcwiise <mcwiise@gmail.com>
2025-07-17 16:02:28 -0500

    chore: improving logs in repository level

--- src/controllers/v1/email.controller.ts
+++ src/controllers/v1/email.controller.ts
@@ -52,7 +52,7 @@ export class EmailController {
           );
           return EmailValidationDto.from(
             onboarding.getOnboardingId(),
-            emailStatus,
+            onboarding.getEmail(),
           );
         }
       });
